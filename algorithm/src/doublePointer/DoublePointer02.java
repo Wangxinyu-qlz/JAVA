75d7260823886d65ba6ce108cba7ba3c5bd9fac0
Qiaolezi <39477671+Finn-Neo@users.noreply.github.com>
2024-03-06 18:48:57 +0800

    差分 双指针

--- algorithm/src/doublePointer/DoublePointer02.java
+++ algorithm/src/doublePointer/DoublePointer02.java
@@ -1,7 +1,4 @@
 package doublePointer;
-
-import org.junit.jupiter.api.Test;
-
 import java.util.Scanner;
 
 /**
@@ -11,7 +8,7 @@ import java.util.Scanner;
  * @description: 给定一个长度为的整数序列，请找出最长的不包含重复数字的连续子序列，输出它的长度。
  * 输入格式
  * 第一行包含整数n.
- * 第二行包含个整数(均在0-100000范围内)，表示整数序列。
+ * 第二行包含个整数(均在 0-100000 范围内)，表示整数序列。
  * 输出格式
  * 共一行，包含一个整数，表示最长的不包含重复数字的连续子序列的长度。
  * 数据范围
@@ -28,6 +25,7 @@ public class DoublePointer02 {
 		int res = 0;
 		int[] s = new int[n+10];
 		for(int i=0, j=0; i<n; i++) {
+			//TODO 使用数组记录元素的数量
 			s[q[i]]++;//记录i指针 所指的元素 的数量
 			while(s[q[i]] > 1) {//如果i指针所指元素数量>1，遇到了重复的元素，j右移，跳过
 				s[q[j]] --;//j指针所指元素数量-1
