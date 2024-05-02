c06aa680de12dfee9f4b633a690dcd18e4d87f1a
qiaolezi <2463853170@qq.com>
2024-05-02 17:28:06 +0800

    where if choose

---
+++ myBatis/dynamic-sql/src/main/java/mapper/MonsterMapper.java
@@ -0,0 +1,22 @@
+package mapper;
+
+import entity.Monster;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @program: mybatis
+ * @author: Qiaolezi
+ * @create: 2024-04-30 16:22
+ * @description: 用于声明操作monster表的方法
+ * 通过注解  xml 文件实现
+ **/
+public interface MonsterMapper {
+	List<Monster> getMonsterByAge(@Param(value = "age") Integer age);
+
+	List<Monster> getMonsterByIdAndName(Monster monster);
+
+	List<Monster> getMonsterByIdOrName_choose(Map<String ,Object> map);
+}
---
+++ myBatis/dynamic-sql/src/test/java/mapper/MonsterMapperTest.java
@@ -0,0 +1,87 @@
+package mapper;
+
+import entity.Monster;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Before;
+import org.junit.Test;
+import util.MyBatisUtils;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+/**
+ * @program: mybatis
+ * @author: Qiaolezi
+ * @create: 2024-04-30 16:47
+ * @description:
+ **/
+public class MonsterMapperTest {
+	private SqlSession sqlSession;
+	private MonsterMapper monsterMapper;
+
+	@Before
+	public void init() {
+		sqlSession = MyBatisUtils.getSqlSession();
+		monsterMapper = sqlSession.getMapper(MonsterMapper.class);
+	}
+
+	@Test
+	public void test1() {
+		System.out.println("test()......");
+	}
+
+
+	@Test
+	public void selectAllMonsterIf() {
+		List<Monster> monsters = monsterMapper.getMonsterByAge(100);
+		for (Monster monster : monsters) {
+			System.out.println("monster=" + monster);
+		}
+
+		if (sqlSession != null) {
+			sqlSession.close();
+		}
+
+		System.out.println("查询完毕~");
+	}
+
+
+	@Test
+	public void selectAllMonsterIfWhere() {
+		Monster monster = new Monster();
+		monster.setId(1);
+		//monster.setName("牛魔王");
+		List<Monster> monsters = monsterMapper.getMonsterByIdAndName(monster);
+
+		for (Monster m : monsters) {
+			System.out.println("m=" + m);
+		}
+
+		if (sqlSession != null) {
+			sqlSession.close();
+		}
+
+		System.out.println("查询完毕~");
+	}
+
+
+	@Test
+	public void getMonsterByIdOrName_choose() {
+		Map<String, Object> map = new HashMap<>();
+		//map.put("name", "牛魔王");
+		//map.put("id", 10);
+		List<Monster> monsters = monsterMapper.getMonsterByIdOrName_choose(map);
+
+		for (Monster m : monsters) {
+			System.out.println("m=" + m);
+		}
+
+		if (sqlSession != null) {
+			sqlSession.close();
+		}
+
+		System.out.println("查询完毕~");
+	}
+}
