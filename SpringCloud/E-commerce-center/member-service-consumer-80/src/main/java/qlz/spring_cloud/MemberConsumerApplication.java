5ab66fcb1a8b015e4437b175d1ef9a55be5fb1e3
qiaolezi <2463853170@qq.com>
2024-06-06 11:39:27 +0800

    配置服务注册和发现 Eureka

--- SpringCloud/E-commerce-center/member-service-consumer-80/src/main/java/qlz/spring_cloud/MemberConsumerApplication.java
+++ SpringCloud/E-commerce-center/member-service-consumer-80/src/main/java/qlz/spring_cloud/MemberConsumerApplication.java
@@ -3,6 +3,7 @@ package qlz.spring_cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @program: E-commerce-center
@@ -13,6 +14,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 //如果 member-service-consumer-80 启动报错: springBoot 启动 If you want an embedded
 //database (H2, HSQL or Derby), please put it on the classpath.
 //加入排除 DataSourceAutoConfiguration 自动配置
+@EnableEurekaClient
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MemberConsumerApplication {
 	public static void main(String[] args) {
