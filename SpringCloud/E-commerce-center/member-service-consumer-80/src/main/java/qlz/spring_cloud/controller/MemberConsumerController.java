55087aa09332fb473996a99fe87e75a5eae7881d
qiaolezi <2463853170@qq.com>
2024-06-06 10:44:24 +0800

    抽取公共模块作为api，使用maven打包调用

--- SpringCloud/E-commerce-center/member-service-consumer-80/src/main/java/qlz/spring_cloud/controller/MemberConsumerController.java
+++ SpringCloud/E-commerce-center/member-service-consumer-80/src/main/java/qlz/spring_cloud/controller/MemberConsumerController.java
@@ -1,6 +1,5 @@
 package qlz.spring_cloud.controller;
 
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
