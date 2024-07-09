bc3c77f4f0b44dc1e58b3a36819fed5bdfe1d155
qiaolezi <2463853170@qq.com>
2024-07-09 11:07:28 +0800

    模块搭建 sql

---
+++ sky-take-out/sky-server/src/main/java/qlz/sky/SkyApplication.java
@@ -0,0 +1,17 @@
+package qlz.sky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: sky-take-out
+ * @author: Qiaolezi
+ * @create: 2024-07-09 11:03
+ * @description:
+ **/
+@SpringBootApplication
+public class SkyApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SkyApplication.class, args);
+	}
+}
