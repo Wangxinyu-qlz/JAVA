77e1e9b1957b9cb34ea51c548e39e4043ff31b15
qiaolezi <2463853170@qq.com>
2024-07-09 10:36:54 +0800

    project init

---
+++ sky-take-out/sky-common/src/test/java/qlz/skycommon/SkyCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package qlz.skycommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ sky-take-out/sky-server/src/test/java/qlz/skyserver/SkyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package qlz.skyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
