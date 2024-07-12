ed952b6ba0673b09620c53b3136edc18e58a67dd
qiaolezi <2463853170@qq.com>
2024-07-12 11:06:26 +0800

    菜品分页查询

--- sky-take-out/sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -1,16 +1,15 @@
-package com.sky.config;
+package com.sky.controller.admin;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @program: sky-take-out
@@ -20,9 +19,9 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 @RequestMapping("/admin/dish")
-@Api(tags = "菜品管理")
+@Api(tags = "菜品相关接口")
 @Slf4j
-public class DishControler {
+public class DishController {
 	@Autowired
 	private DishService dishService;
 	@PostMapping
@@ -31,5 +30,12 @@ public class DishControler {
 		log.info("新增菜品：{}", dishDTO);
 		dishService.saveWithFlavor(dishDTO);
 		return Result.success();
+	}
+
+	@GetMapping("/page")
+	public Result pageQuery(DishPageQueryDTO dishPageQueryDTO) {
+		log.info("菜品分页查询：{}", dishPageQueryDTO);
+		PageResult pageResult = dishService.pageQuery(dishPageQueryDTO);
+		return Result.success(pageResult);
 	}
 }
--- sky-take-out/sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,6 +1,8 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 
 /**
  * @program: sky-take-out
@@ -14,4 +16,6 @@ public interface DishService {
 	 * @param dishDTO
 	 */
 	public void saveWithFlavor(DishDTO dishDTO);
+
+	PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
