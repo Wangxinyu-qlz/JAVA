077be1b0d747e7a20926cfaf0ba6c9b64997be0b
qiaolezi <2463853170@qq.com>
2024-07-22 16:47:52 +0800

    历史订单分页查询

--- sky-take-out/sky-server/src/main/java/com/sky/mapper/OrderDetailMapper.java
+++ sky-take-out/sky-server/src/main/java/com/sky/mapper/OrderDetailMapper.java
@@ -2,6 +2,7 @@ package com.sky.mapper;
 
 import com.sky.entity.OrderDetail;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -19,4 +20,12 @@ public interface OrderDetailMapper {
 	 * @param orderDetailList
 	 */
 	void insertBatch(List<OrderDetail> orderDetailList);
+
+	/**
+	 * 根据orderId查询订单详细信息
+	 * @param id
+	 * @return
+	 */
+	@Select("select * from order_detail where order_id = #{orderId}")
+	List<OrderDetail> getByOrderId(Long id);
 }
--- sky-take-out/sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -1,8 +1,11 @@
 package com.sky.service.impl;
 
 import com.alibaba.fastjson.JSONObject;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.constant.MessageConstant;
 import com.sky.context.BaseContext;
+import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.dto.OrdersPaymentDTO;
 import com.sky.dto.OrdersSubmitDTO;
 import com.sky.entity.*;
@@ -10,13 +13,16 @@ import com.sky.exception.AddressBookBusinessException;
 import com.sky.exception.OrderBusinessException;
 import com.sky.exception.ShoppingCartBusinessException;
 import com.sky.mapper.*;
+import com.sky.result.PageResult;
 import com.sky.service.OrderService;
 import com.sky.utils.WeChatPayUtil;
 import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
+import com.sky.vo.OrderVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -153,5 +159,38 @@ public class OrderServiceImpl implements OrderService {
 				.build();
 
 		orderMapper.update(orders);
+	}
+
+	/**
+	 * 历史订单分页查询
+	 * @param pageNum
+	 * @param pageSize
+	 * @param status
+	 * @return
+	 */
+	@Override
+	public PageResult pageQuery4User(int pageNum, int pageSize, Integer status) {
+		PageHelper.startPage(pageNum, pageSize);
+		OrdersPageQueryDTO ordersPageQueryDTO = new OrdersPageQueryDTO();
+
+		ordersPageQueryDTO.setUserId(BaseContext.getCurrentId());
+		ordersPageQueryDTO.setStatus(status);
+
+		Page<Orders> page = orderMapper.pageQuery(ordersPageQueryDTO);
+		ArrayList<OrderVO> orderVOList = new ArrayList<>();
+		if(page != null && page.getTotal() > 0) {
+			for(Orders order : page) {
+				Long id = order.getId();
+				List<OrderDetail> orderDetailList = orderDetailMapper.getByOrderId(id);
+
+				OrderVO orderVO = new OrderVO();
+				BeanUtils.copyProperties(order, orderVO);
+				orderVO.setOrderDetailList(orderDetailList);
+
+				orderVOList.add(orderVO);
+			}
+		}
+
+		return new PageResult(page.getTotal(), orderVOList);
 	}
 }
