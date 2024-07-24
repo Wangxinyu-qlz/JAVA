9ee871a07c60c333fbb22c5176c090bc095c9688
qiaolezi <2463853170@qq.com>
2024-07-24 18:00:47 +0800

    销量统计

--- sky-take-out/sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-take-out/sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.dto.GoodsSalesDTO;
 import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Delete;
@@ -86,4 +87,12 @@ public interface OrderMapper {
 	 * @return
 	 */
 	Integer countByMap(Map map);
+
+	/**
+	 * 销量排名top10
+	 * @param begin
+	 * @param end
+	 * @return
+	 */
+	List<GoodsSalesDTO> getSalesTop(LocalDateTime begin, LocalDateTime end);
 }
