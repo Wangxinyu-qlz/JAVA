9aada38a749841c049d5a7eeab72ea02d03bbd8b
qiaolezi <2463853170@qq.com>
2024-08-14 17:09:42 +0800

    uniqueInstance = new Singleton(); 字节码分析

--- design_pattern/src/creation_based/singleton_pattern/LazybonesDoubleCheckedLock.java
+++ design_pattern/src/creation_based/singleton_pattern/LazybonesDoubleCheckedLock.java
@@ -7,7 +7,7 @@ package creation_based.singleton_pattern;
  * @description:
  **/
 //线程安全，双重校验锁
-public class LazybonesDoubleCheckLock {
+public class LazybonesDoubleCheckedLock {
 	//uniqueInstance 采用 volatile 关键字修饰也是很有必要的。
 	// uniqueInstance = new Singleton();
 	// 这段代码其实是分为三步执行。
@@ -25,13 +25,13 @@ public class LazybonesDoubleCheckLock {
 	//保证可见性：volatile 还确保了 uniqueInstance 的最新值对所有线程是可见的。
 	// 当一个线程修改了 uniqueInstance，其他线程立即能够看到修改后的值，而不会使用缓存中的过期数据。
 
-	private volatile static LazybonesDoubleCheckLock uniqueZInstance;
+	private volatile static LazybonesDoubleCheckedLock uniqueZInstance;
 
-	private LazybonesDoubleCheckLock() {}
+	private LazybonesDoubleCheckedLock() {}
 
 	//双重校验锁先判断 uniqueInstance 是否已经被实例化，
 	// 如果没有被实例化，那么才对实例化语句进行加锁。
-	public static LazybonesDoubleCheckLock getUniqueZInstance() {
+	public static LazybonesDoubleCheckedLock getUniqueZInstance() {
 		/*
 		当第一个线程走到第一次检查时发现对象为空，然后进入锁，
 		第二次检查时也为空，那么就去创建对象，但是这个时候又来了一个线程来到了第一次检查，
@@ -64,8 +64,8 @@ public class LazybonesDoubleCheckLock {
 					因此，使用类锁时，所有对这个类的同步操作都会被该锁阻塞，
 					意味着无论有多少个实例对象，使用类锁时都只能有一个线程能够进入同步代码块。
 			锁范围：
-				当一个线程获取了 LazybonesDoubleCheckLock.class 的锁后，
-				其他任何试图进入同一类的 synchronized (LazybonesDoubleCheckLock.class)
+				当一个线程获取了 LazybonesDoubleCheckedLock.class 的锁后，
+				其他任何试图进入同一类的 synchronized (LazybonesDoubleCheckedLock.class)
 				代码块的线程都会被阻塞。
 				这也意味着，使用类锁时，锁的范围是整个类，而不仅仅是某个实例，
 				因此这种锁通常用于需要确保对类级别的资源（如静态变量）进行同步访问的情况。
@@ -74,12 +74,24 @@ public class LazybonesDoubleCheckLock {
 				适用于单例模式中，确保在并发环境下只有一个实例被创建。
 			 */
 			//类锁：锁住当前类  （锁住静态方法  锁住静态变量）
-			synchronized (LazybonesDoubleCheckLock.class) {
+			synchronized (LazybonesDoubleCheckedLock.class) {
 				if(uniqueZInstance == null) {
-					uniqueZInstance = new LazybonesDoubleCheckLock();
+					//17 new #8 <creation_based/singleton_pattern/LazybonesDoubleCheckedLock>
+					//20 dup
+					//21 invokespecial #13 <creation_based/singleton_pattern/LazybonesDoubleCheckedLock.<init> : ()V>
+					//24 putstatic #7 <creation_based/singleton_pattern/LazybonesDoubleCheckedLock.uniqueZInstance : Lcreation_based/singleton_pattern/LazybonesDoubleCheckedLock;>
+					//new 指令创建一个LazybonesDoubleCheckedLock类的新实例。
+					//dup 复制这个新创建的实例引用，以便将其同时用于构造函数调用和putstatic指令。
+					//invokespecial 调用构造函数初始化新对象。
+					//putstatic 将新创建的对象存储在uniqueZInstance静态字段中。
+					uniqueZInstance = new LazybonesDoubleCheckedLock();
 				}
 			}
 		}
 		return uniqueZInstance;
+	}
+
+	public static void main(String[] args) {
+		LazybonesDoubleCheckedLock instance = LazybonesDoubleCheckedLock.getUniqueZInstance();
 	}
 }
